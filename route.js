3643e20a836e163b3b57ca66397b2904ee30e59e
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-06-03 11:10:15 +0300

    bvn

--- route.js
+++ route.js
@@ -5,14 +5,13 @@ import { verifiedToken } from "./middlewares/verifiedToken.js";
 
 const router = express.Router();
 
-router.get('/users', getAllUsers);
-// router.get('/users', verifiedToken, getAllUsers);
+router.get('/users', verifiedToken, getAllUsers);
 router.post('/users/register', Register);
 router.post('/users/login', Login);
 router.get('/users/:id', getUser);
 
 router.get('/destinations', getAllDestinations);
-router.post('/destination/add/:user_id', verifiedToken, addDestination); 
+router.post('/destination/add/:user_id', addDestination); 
 router.get('/destinations/:user_id', getDestinationsByUser)
 router.get('/destinations/users/:country',getUserByDestination)
 
