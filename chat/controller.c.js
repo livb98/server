82cfb0b3fa06cd3fc9610e9649e90823f0976a1e
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-06-05 16:46:08 +0300

    f

--- chat/controller.c.js
+++ chat/controller.c.js
@@ -9,7 +9,7 @@ dotenv.config();
 export const getChat = async (req, res) => {
     const { chat_id, sender_id, user2_id } = req.params;
     try {
-        const chat = await _getChat(chat_id, sender_id);
+        const chat = await _getChat(chat_id, sender_id, user2_id);
         res.json(chat);
     } catch (error) {
         console.log(`Chat controller error => ${error}`);
--- chat/model.c.js
+++ chat/model.c.js
@@ -1,57 +1,11 @@
-// import { db } from "../config/config.js";
-
-// export const _getChat = async (chat_id) => {
-//     try {
-//         const chat = await db('message as m')
-//             .select('c.chat_id',
-//                     'm.msg_id',
-//                     'm.message',
-//                     'm.sender_id',
-//                     'u1.username as user1_username', 
-//                     'u1.user_id as user1_id',
-//                     'u2.username as user2_username', 
-//                     'u2.user_id as user2_id')
-//             .join('chat as c','c.chat_id', '=', 'm.chat_id' )
-//             .join('users as u1', 'c.fk_user1', '=', 'u1.user_id')
-//             .join('users as u2', 'c.fk_user2', '=', 'u2.user_id')
-//             .orderBy('m.msg_id')
-//             .where('m.chat_id', chat_id);
-//         return chat;
-//     } catch (error) {
-//         console.error(`Error fetching chat ${error}`);
-//         throw new Error('Failed to fetch chat');
-//     }
-// };
-
-
-// export const _getAllChat = async (user_id) => {
-//     try {
-//         const chatDetails = await db('chat as c')
-//             .select('c.chat_id', 
-//                     'u1.username as user1_username', 
-//                     'u1.user_id as user1_id',
-//                     'u2.username as user2_username', 
-//                     'u2.user_id as user2_id')
-//             .join('users as u1', 'c.fk_user1', '=', 'u1.user_id')
-//             .join('users as u2', 'c.fk_user2', '=', 'u2.user_id')
-//             .where('u1.user_id', user_id)
-//             // .orWhere('u2.user_id', user_id)
-//             .orderBy('c.chat_id');
-//         return chatDetails;
-//     } catch (error) {
-//         console.error('Error fetching all chat details:', error);
-//         throw new Error('Failed to fetch chat details');
-//     }
-// };
 import { db } from "../config/config.js";
 
-export const _getChat = async (chat_id, sender_id) => {
+export const _getChat = async (chat_id, sender_id, user2_id) => {
     try {
         const chat = await db('message as m')
             .select('c.chat_id',
                     'm.msg_id',
                     'm.message',
-                    'm.sender_id',
                     'u1.username as user1_username', 
                     'u1.user_id as user1_id',
                     'u2.username as user2_username', 
@@ -60,7 +14,10 @@ export const _getChat = async (chat_id, sender_id) => {
             .join('users as u1', 'c.fk_user1', '=', 'u1.user_id')
             .join('users as u2', 'c.fk_user2', '=', 'u2.user_id')
             .where('m.chat_id', chat_id)
-            .andWhere('m.sender_id', sender_id)
+            .andWhere(function() {
+                this.where('m.sender_id', sender_id)
+                .orWhere('u2.user_id', user2_id);
+            })
             .orderBy('m.msg_id');
         return chat;
     } catch (error) {
@@ -68,6 +25,7 @@ export const _getChat = async (chat_id, sender_id) => {
         throw new Error('Failed to fetch chat');
     }
 };
+
 
 export const _getAllChat = async (user_id) => {
     try {
