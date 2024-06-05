02b32e1cc9f18741ab200bb168d83a22b3a78785
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-06-05 15:03:26 +0300

    lkj

--- chat/controller.c.js
+++ chat/controller.c.js
@@ -1,30 +1,61 @@
+// import { _sendMessage, _getAllChat, _getChat, _newChat } from "./model.c.js";
+// import { getUser } from "../users/controller.u.js";
+// import bcrypt from "bcrypt";
+// import dotenv from "dotenv";
+// import jwt from 'jsonwebtoken'
+// dotenv.config();
+
+// export const getChat = async(req,res) =>{
+//     const {chat_id, sender_id} = req.params
+//     try {
+//         const chat = await _getChat(chat_id)
+//         res.json(chat)
+
+//     } catch (error) {
+//         console.log(`chat cont => ${error}`);
+//         res.status(404).json({msg:'not found'})
+//     }
+// }
+
+// export const getAllChat = async(req,res) => {
+//     const { user_id } = req.params;
+//     try {
+//         const chatDetails = await _getAllChat(user_id);
+//         res.json(chatDetails);
+//     } catch (error) {
+//         console.log(`Chat details controller error => ${error}`);
+//         res.status(404).json({ msg: 'Not found' });
+//     }
+// }
 import { _sendMessage, _getAllChat, _getChat, _newChat } from "./model.c.js";
 import { getUser } from "../users/controller.u.js";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
+
 dotenv.config();
 
-export const getChat = async(req,res) =>{
-    const {chat_id, sender_id} = req.params
+// Function to get a specific chat's messages
+export const getChat = async (req, res) => {
+    const { chat_id, sender_id } = req.params;
     try {
-        const chat = await _getChat(chat_id)
-        res.json(chat)
-
+        const chat = await _getChat(chat_id);
+        res.json(chat);
     } catch (error) {
-        console.log(`chat cont => ${error}`);
-        res.status(404).json({msg:'not found'})
+        console.log(`Chat controller error => ${error}`);
+        res.status(404).json({ msg: 'Chat not found' });
     }
 }
 
-export const getAllChat = async(req,res) => {
+// Function to get all chat details for a specific user
+export const getAllChat = async (req, res) => {
     const { user_id } = req.params;
     try {
         const chatDetails = await _getAllChat(user_id);
         res.json(chatDetails);
     } catch (error) {
         console.log(`Chat details controller error => ${error}`);
-        res.status(404).json({ msg: 'Not found' });
+        res.status(404).json({ msg: 'Chat details not found' });
     }
 }
 
